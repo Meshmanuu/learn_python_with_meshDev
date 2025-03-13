b5737ef685e17d1a2d57fe54ac1688277d9c7e18
Meshmanuu <meshanyangu@gmail.com>
2025-03-13 16:59:36 +0300

    greetings app done

---
+++ week 1/code-challenge/greetings.py
@@ -0,0 +1,7 @@
+#user input
+userName= input("Whats Your Name?")
+userFavouriteColour= input("Enter Your Favourite Colour")
+ 
+greetings= f"Hello, {userName}! Your favorite color, {userFavouriteColour} ,is awesome!”"
+
+print(greetings)
